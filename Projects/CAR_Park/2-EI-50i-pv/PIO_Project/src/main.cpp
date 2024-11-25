f5a502bcfef44be732e9952c5f4d782b1acb6606
Valentin <valentin.faucheux02@gmail.com>
2024-11-25 11:50:03 +0100

    Ajout de build flags et renommage dossier

--- Projects/CAR_Park/2-EI-50i-pv/PIO_Project/src/main.cpp
+++ Projects/CAR_Park/2-EI-50i-pv/PIO_Project/src/main.cpp
@@ -7,73 +7,85 @@
 
 #define CONFIG_ESP32_CAMERA_DEBUG 1
 
-// Choose your correct Board:
-//#define CAMERA_MODEL_ESP_SENSE 
-#define CAMERA_MODEL_AI_THINKER 
-//#define CAMERA_MODEL_ESP_S3_EYE
+#ifdef EYE
+    #define PWDN_GPIO_NUM    -1
+    #define RESET_GPIO_NUM   -1
+    #define XCLK_GPIO_NUM    15
+    #define SIOD_GPIO_NUM    4
+    #define SIOC_GPIO_NUM    5
 
-#if defined(CAMERA_MODEL_ESP_S3_EYE)
-#define PWDN_GPIO_NUM    -1
-#define RESET_GPIO_NUM   -1
-#define XCLK_GPIO_NUM    15
-#define SIOD_GPIO_NUM    4
-#define SIOC_GPIO_NUM    5
+    #define Y9_GPIO_NUM      16
+    #define Y8_GPIO_NUM      17
+    #define Y7_GPIO_NUM      18
+    #define Y6_GPIO_NUM      12
+    #define Y5_GPIO_NUM      10
+    #define Y4_GPIO_NUM      8
+    #define Y3_GPIO_NUM      9
+    #define Y2_GPIO_NUM      11
 
-#define Y9_GPIO_NUM      16
-#define Y8_GPIO_NUM      17
-#define Y7_GPIO_NUM      18
-#define Y6_GPIO_NUM      12
-#define Y5_GPIO_NUM      10
-#define Y4_GPIO_NUM      8
-#define Y3_GPIO_NUM      9
-#define Y2_GPIO_NUM      11
+    #define VSYNC_GPIO_NUM   6
+    #define HREF_GPIO_NUM    7
+    #define PCLK_GPIO_NUM    13
 
-#define VSYNC_GPIO_NUM   6
-#define HREF_GPIO_NUM    7
-#define PCLK_GPIO_NUM    13
+#elif defined(SENSE)
+    #define PWDN_GPIO_NUM -1
+    #define RESET_GPIO_NUM -1
+    #define XCLK_GPIO_NUM 10
+    #define SIOD_GPIO_NUM 40
+    #define SIOC_GPIO_NUM 39
 
-#elif defined(CAMERA_MODEL_ESP_SENSE)
-#define PWDN_GPIO_NUM -1
-#define RESET_GPIO_NUM -1
-#define XCLK_GPIO_NUM 10
-#define SIOD_GPIO_NUM 40
-#define SIOC_GPIO_NUM 39
+    #define Y9_GPIO_NUM 48
+    #define Y8_GPIO_NUM 11
+    #define Y7_GPIO_NUM 12
+    #define Y6_GPIO_NUM 14
+    #define Y5_GPIO_NUM 16
+    #define Y4_GPIO_NUM 18
+    #define Y3_GPIO_NUM 17
+    #define Y2_GPIO_NUM 15
 
-#define Y9_GPIO_NUM 48
-#define Y8_GPIO_NUM 11
-#define Y7_GPIO_NUM 12
-#define Y6_GPIO_NUM 14
-#define Y5_GPIO_NUM 16
-#define Y4_GPIO_NUM 18
-#define Y3_GPIO_NUM 17
-#define Y2_GPIO_NUM 15
+    #define VSYNC_GPIO_NUM 38
+    #define HREF_GPIO_NUM 47
+    #define PCLK_GPIO_NUM 13
 
-#define VSYNC_GPIO_NUM 38
-#define HREF_GPIO_NUM 47
-#define PCLK_GPIO_NUM 13
+#elif defined(CAM)
+    #define PWDN_GPIO_NUM     32
+    #define RESET_GPIO_NUM    -1
+    #define XCLK_GPIO_NUM      0
+    #define SIOD_GPIO_NUM     26
+    #define SIOC_GPIO_NUM     27
 
-#elif defined(CAMERA_MODEL_AI_THINKER)
-#define PWDN_GPIO_NUM     32
-#define RESET_GPIO_NUM    -1
-#define XCLK_GPIO_NUM      0
-#define SIOD_GPIO_NUM     26
-#define SIOC_GPIO_NUM     27
+    #define Y9_GPIO_NUM       35
+    #define Y8_GPIO_NUM       34
+    #define Y7_GPIO_NUM       39
+    #define Y6_GPIO_NUM       36
+    #define Y5_GPIO_NUM       21
+    #define Y4_GPIO_NUM       19
+    #define Y3_GPIO_NUM       18
+    #define Y2_GPIO_NUM        5
 
-#define Y9_GPIO_NUM       35
-#define Y8_GPIO_NUM       34
-#define Y7_GPIO_NUM       39
-#define Y6_GPIO_NUM       36
-#define Y5_GPIO_NUM       21
-#define Y4_GPIO_NUM       19
-#define Y3_GPIO_NUM       18
-#define Y2_GPIO_NUM        5
-
-#define VSYNC_GPIO_NUM    25
-#define HREF_GPIO_NUM     23
-#define PCLK_GPIO_NUM     22
+    #define VSYNC_GPIO_NUM    25
+    #define HREF_GPIO_NUM     23
+    #define PCLK_GPIO_NUM     22
 
 #else
-#error "Camera model not selected"
+    #define PWDN_GPIO_NUM     0
+    #define RESET_GPIO_NUM    0
+    #define XCLK_GPIO_NUM     0
+    #define SIOD_GPIO_NUM     0
+    #define SIOC_GPIO_NUM     0
+
+    #define Y9_GPIO_NUM       0
+    #define Y8_GPIO_NUM       0
+    #define Y7_GPIO_NUM       0
+    #define Y6_GPIO_NUM       0
+    #define Y5_GPIO_NUM       0
+    #define Y4_GPIO_NUM       0
+    #define Y3_GPIO_NUM       0
+    #define Y2_GPIO_NUM       0
+
+    #define VSYNC_GPIO_NUM    0
+    #define HREF_GPIO_NUM     0
+    #define PCLK_GPIO_NUM     0
 #endif
 
 /* Constant defines -------------------------------------------------------- */
